a33ed98fd314df223391d8983daafe6953cb185b
karlhwang <karl900410@gmail.com>
2021-08-01 17:56:55 +0900

    append slack_bot to fb_api

--- sample_karl.py
+++ sample_karl.py
@@ -24,35 +24,71 @@ from facebook_business.adobjects.adsinsights import AdsInsights
 from facebook_business.adobjects.adaccount import AdAccount
 import json
 import sys
-# Replace this with the place you installed facebookads using pip
-sys.path.append('/usr/local/lib/python3.9/site-packages/site-packages')
-# same as above
-sys.path.append(
-    '/usr/local/lib/python3.9/site-packages/facebook_business-3.0.0-py2.7.egg-info')
+import pandas as pd
+import requests
+import datetime
 
+def slack_alarm_fb_info(fb_info, ACCESS_TOKEN, channel_id):
+    message = fb_info.to_string()
+    data = {'Content-Type': 'application/x-www-form-urlencoded',
+            'token': ACCESS_TOKEN,
+            'channel': channel_id, 
+            'text': message
+            } 
+    try:
+        URL = "https://slack.com/api/chat.postMessage"
+        res = requests.post(URL, data=data)
+    except:
+        print(3)
 
-access_token = config.access_token
-ad_account_id = config.ad_account_id
-app_secret = config.app_secret
-app_id = config.app_id
+def get_date_range():
+    today = datetime.date.today()
+    seven_days_ago = today - datetime.timedelta(days=6)
+    today = today.strftime('%Y-%m-%d')
+    seven_days_ago = seven_days_ago.strftime('%Y-%m-%d')
+    return seven_days_ago, today
 
-FacebookAdsApi.init(access_token=access_token)
+if __name__ == "__main__":
+    # Replace this with the place you installed facebookads using pip
+    sys.path.append('/usr/local/lib/python3.9/site-packages/site-packages')
+    # same as above
+    sys.path.append(
+        '/usr/local/lib/python3.9/site-packages/facebook_business-3.0.0-py2.7.egg-info')
 
-fields = [
+    fb_access_token = config.FACEBOOK_ACCESS_TOKEN
+    fb_ad_account_id = config.FACEBOOK_AD_ACCOUNT_ID
+    fb_app_secret = config.FACEBOOK_APP_SECRET
+    fb_app_id = config.FACEBOOK_APP_ID
 
-    'clicks',
-    'campaign_name',
-    'spend'
-]
-params = {
-    'time_range': {'since': '2021-07-20', 'until': '2021-07-20'},
-    # 'level': 'adset',
-    'level': 'campaign'
+    slack_access_token = config.SLACK_ACCESS_TOKEN
+    slack_channel_id = config.SLACK_CHANNEL_ID
 
-}
+    FacebookAdsApi.init(access_token=fb_access_token)
 
+    seven_days_ago, today = get_date_range()
+    fields = [
+        'campaign_name',
+        'clicks',
+        'spend',
+    ]
+    params = {
+        'time_range': {'since': seven_days_ago, 'until': today},
+        # 'level': 'adset',
+        'level': 'campaign'
+    }
 
-print(AdAccount(ad_account_id).get_insights(
-    fields=fields,
-    params=params,
-))
+    results =  AdAccount(fb_ad_account_id).get_insights(
+        fields=fields,
+        params=params,
+    )
+  
+    df = pd.DataFrame(columns=['campaing_name', 'clicks','spend','date_start','date_stop'])
+    contents_list = []
+    for result in results:
+        for key, value in result.items():
+            contents_list.append(value)
+        tmp_series = pd.Series(contents_list, index=df.columns)
+        df = df.append(tmp_series, ignore_index=True)
+        contents_list = []
+    fb_info = df
+    slack_alarm_fb_info(fb_info, slack_access_token, slack_channel_id)
